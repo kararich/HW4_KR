f672dd831386685b9e268ad44250c7a1a1acff36
Kara Rich <kara.rich@kellogg.northwestern.edu>
2024-03-12 01:29:19 +0000

    part 3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,12 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by({ "email" => params["email"] })
-    if @user != nil
-      if BCrypt::Password.new(@user["password"]) == params["password"]
-        session["user_id"] = @user["id"]
-        flash["notice"] = "Hello."
-        redirect_to "/places"
-      else
-        flash["notice"] = "Nope."
-        redirect_to "/login"
-      end
+
+    if @user.present? && BCrypt::Password.new(@user["password"]) == params["password"]
+      session["user_id"] = @user["id"]
+      cookies.signed[:user_id] = { value: @user["id"], expires: 1.week.from_now }
+      flash["notice"] = "Hello."
+      redirect_to "/places"
     else
       flash["notice"] = "Nope."
       redirect_to "/login"
@@ -21,8 +18,10 @@ class SessionsController < ApplicationController
 
   def destroy
     session["user_id"] = nil
+    cookies.delete(:user_id)
     flash["notice"] = "Goodbye."
     redirect_to "/login"
   end
 end
+
   
